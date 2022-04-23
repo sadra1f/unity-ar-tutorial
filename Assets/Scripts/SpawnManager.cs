6f20f393ad04383f8b6afbe236aaeb0c98f82d3b
Sadra Saderi <sadrassx@gmail.com>
2022-04-23 17:01:06 +0430

    Spawn object

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class SpawnManager : MonoBehaviour
+{
+    [SerializeField] Camera cam;
+    [SerializeField] ARRaycastManager raycastManager;
+    [SerializeField] GameObject obj;
+
+    List<ARRaycastHit> hits;
+    GameObject spawnedObj;
+
+    private void Awake()
+    {
+        hits = new List<ARRaycastHit>();
+        spawnedObj = null;
+    }
+
+    private void Start()
+    {
+        if (!cam)
+            cam = FindObjectOfType<Camera>();
+        if (!raycastManager)
+            raycastManager = FindObjectOfType<ARRaycastManager>();
+    }
+
+    private void Update()
+    {
+        if (Input.touchCount == 0) return;
+
+        RaycastHit hit;
+
+        Touch touch = Input.GetTouch(0);
+        Ray ray = cam.ScreenPointToRay(touch.position);
+
+
+        if (raycastManager.Raycast(touch.position, hits))
+        {
+            if (
+                touch.phase == TouchPhase.Began
+                && !spawnedObj
+                && Physics.Raycast(ray, out hit)
+            )
+            {
+                SpawnObject(hits[0].pose.position);
+            }
+            else if (
+                (
+                    touch.phase == TouchPhase.Moved
+                    || touch.phase == TouchPhase.Began
+                )
+                && spawnedObj
+            )
+            {
+                spawnedObj.transform.position = hits[0].pose.position;
+            }
+        }
+    }
+
+    private void SpawnObject(Vector3 position)
+    {
+        if (spawnedObj) Destroy(spawnedObj);
+        spawnedObj = Instantiate(obj, position, Quaternion.identity);
+    }
+}
